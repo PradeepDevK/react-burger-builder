4b969d4c43aec0617a6621f0c86aa63dba1513f5
ACCIONLABS\Al1430 <pradeepjack555@gmail.com>
2020-03-31 14:06:05 +0530

    Added routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,31 +90,17 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        this.setState({loading:true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Pradeep Raj',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '632001',
-                    country: 'India'
-                },
-                email: 'test@est.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => { 
-                console.log(response);
-                this.setState({loading:false, purchasing: false});
-            })
-            .catch(error => {
-                console.log(error);
-                this.setState({loading:false, purchasing: false});
-            });
+
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i)+ '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname : '/checkout',
+            search: '?' +  queryString
+        });
     }
 
     render () {
